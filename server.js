155732da7fd652ac4c47e9d8ea2f1c2df88dacea
George Aureliu Stan <gestan@adobe.com>
2016-01-09 16:49:15 +0200

    adding socket io to package

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 
 app.use('/', express.static(__dirname + '/public'));
 app.listen(3000, function() { console.log('listening')});
-// server.listen(process.env.PORT || 3000);
