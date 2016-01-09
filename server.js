5cc9c369456092e1cab01f4bd3455afcca580314
George Aureliu Stan <gestan@adobe.com>
2016-01-09 17:23:41 +0200

    more refactor

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-
-app.use('/', express.static(__dirname + '/public'));
 var port = process.env.PORT || 3000;
 
+app.use('/', express.static(__dirname + '/public'));
 app.listen(port, function() { console.log('listening')});
