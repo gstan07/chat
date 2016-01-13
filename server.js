c3ac963f07349c5c593e545c5f4cd92f096d0758
George Aureliu Stan <gestan@adobe.com>
2016-01-13 11:33:59 +0200

    removing pingtimeout and pinginterval

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server,{
-	'pingInterval': 360000,
-	'pingTimeout': 360000
+	// 'pingInterval': 360000,
+	// 'pingTimeout': 360000
 });
 
 var port = process.env.PORT || 3000;
